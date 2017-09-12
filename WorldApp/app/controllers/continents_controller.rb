21f36be262d3680bf3a35cd6b32a898ab7f66e7f
Karen Baque <k.baque@aol.com>
2017-09-12 11:02:06 -0400

    add photos to countries

--- WorldApp/app/controllers/continents_controller.rb
+++ WorldApp/app/controllers/continents_controller.rb
@@ -3,4 +3,9 @@ class ContinentsController < ApplicationController
     def index 
         @continents = Continent.all
     end 
+
+    def show 
+        @continent = Continent.find_by(id: params[:id])
+        @countries = @continent.countries
+    end 
 end
--- WorldApp/app/controllers/countries_controller.rb
+++ WorldApp/app/controllers/countries_controller.rb
@@ -1,5 +1,11 @@
 class CountriesController < ApplicationController
+
     def index 
         @contries = Country.all
+    end 
+
+    def show
+        @country = Country.find_by(id: params[:id])
+        @continent = @country.continent
     end 
 end
